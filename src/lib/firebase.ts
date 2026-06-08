0783699f892f8b36c053d0b9e0f113a7d5677a01
AlyEl-deen <dentalmax007@gmail.com>
2026-06-09 02:00:40 +0300

    add firebase.ts real configs

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,18 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  firestoreDatabaseId: import.meta.env.VITE_FIRESTORE_DATABASE_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyBWpVWIhanYY51Z1dklCKHlcIIRCtXfXr8",
+  authDomain: "wmd-office-web.firebaseapp.com",
+  projectId: "wmd-office-web",
+  storageBucket: "wmd-office-web.firebasestorage.app",
+  messagingSenderId: "1062269186222",
+  appId: "1:1062269186222:web:d9ca69db4d249e7c77bd3f",
+  measurementId: "G-P8G2GJNXN1"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
